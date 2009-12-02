a722ed5d2b48119677a5fd52950efb16ce0ad3d6
Federico <federico.marani@ymail.com>
2009-12-02 18:39:59 +0000

    second update

--- src/main/php/Controller.php
+++ src/main/php/Controller.php
@@ -2,10 +2,30 @@
 
 class riddance_UnitOfWork
 {
+    /**
+     * @var string
+     */
     public $templateText;
+
+    /**
+     * @var string
+     */
     public $templateHtml;
-    public array $emails;
-    public array $templateVars;
+    
+    /**
+     * @var string
+     */
+    public $subject;
+    
+    /**
+     * @var array
+     */
+    public $emails;
+    
+    /**
+     * @var array
+     */
+    public $templateVars;
 }
 
 
@@ -32,12 +52,25 @@ class riddance_Controller
     protected function prepareForSubmission($unitOfWork)
     {
         // prepare data
+        
+        // 1. extract first level of data in separate var
+        
+        // 2. create a map with all arrays in any level as first level
+        
         //{\"template-text\" : \"bar\", \"template-html\" : \"bar\", \"email\" : \"bar@bar.com\", \"blkdata\" : \"[{\"aaa\" : \"AAA\"}]\", \"data\" : \"{\"zzz\" : \"ZZZ\"}\"}
-
+        $prepared = array(
+          "template-text" => $unitOfWork->templateText,
+          "template-html" => $unitOfWork->templateHtml,
+          "subject" => $unitOfWork->subject,
+          "emails" => $unitOfWork->emails,
+          // add
+        );
+        return $prepared;
     }
 
-    public function release($unitOfWork)
+    public function release(riddance_UnitOfWork $unitOfWork)
     {
+        // CHUNKIFY UNITOFWORK!! (max 300 emails per msg)
         $content = $this->prepareForSubmission($unitOfWork);
 	    $envelope = new messaging_message_JsonMessage($content);
 
