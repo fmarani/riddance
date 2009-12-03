825dc25e7f83459bcf053c5fbf19e80bf166b98b
Federico <federico.marani@ymail.com>
2009-12-03 18:18:59 +0000

    first ver of array preparation for submission

--- src/main/php/Controller.php
+++ src/main/php/Controller.php
@@ -49,21 +49,56 @@ class riddance_Controller
         $c->disconnect();
     }
 
+    private function flatten_array($array) {
+        $size=sizeof($array);
+        $keys=array_keys($array);
+        for($x = 0; $x < $size; $x++) {
+            $element = $array[$keys[$x]];
+
+            if(is_array($element)) {
+                $results = $this->flatten_array($element);
+                    $sr = sizeof($results);
+                    $sk=array_keys($results);
+                for($y = 0; $y < $sr; $y++) {
+                    $flat_array[$sk[$y]] = $results[$sk[$y]];
+                }
+            } else {
+                $flat_array[$keys[$x]] = strval($element);
+            }
+        }
+
+        return $flat_array;
+    }
+
     protected function prepareForSubmission($unitOfWork)
     {
         // prepare data
-        
+        $blockMapContainers = array();
+        $templateMaps = array();
         // 1. extract first level of data in separate var
-        
         // 2. create a map with all arrays in any level as first level
-        
+        foreach ($unitOfWork->templateVars as $mash) {
+            $blockMapContainer = array();
+            $templateMap = array();
+            foreach ($mash as $key => $value) {
+                if (is_array($value)) {
+                    $blockMapContainer[$key] = $value;
+                } else {
+                    $templateMap[$key] = strval($value);
+                }
+            }
+            array_push($blockMapContainers, array_map($this->flatten_array, $blockMapContainer));
+            array_push($templateMaps, $templateMap);
+        }
+
         //{\"template-text\" : \"bar\", \"template-html\" : \"bar\", \"email\" : \"bar@bar.com\", \"blkdata\" : \"[{\"aaa\" : \"AAA\"}]\", \"data\" : \"{\"zzz\" : \"ZZZ\"}\"}
         $prepared = array(
           "template-text" => $unitOfWork->templateText,
           "template-html" => $unitOfWork->templateHtml,
           "subject" => $unitOfWork->subject,
           "emails" => $unitOfWork->emails,
-          // add
+          "blkdata" => $blockMapContainers,
+          "data" => $templateMaps
         );
         return $prepared;
     }
