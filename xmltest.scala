cf7e6f62ed20080ec561ec43b8aea40e06096003
Federico Marani <federico.marani@ymail.com>
2010-01-26 00:28:31 +0000

    xml test code finished

--- xmltest.scala
+++ xmltest.scala
@@ -1,8 +1,10 @@
 
+import scala.xml._
+
 val x = <riddance req="mailout">
 <template>
     <text>testestest</text>
-    <html><![CDATA["<html><body>ayo!</body></html>"]]></html>
+    <html><![CDATA[<html><body>ayo!</body></html>]]></html>
 </template>
 <iterations>
 <unit email="flagzeta@gmail.com">
@@ -32,36 +34,26 @@ val x = <riddance req="mailout">
 </iterations>
 </riddance>
 
-val tt = x\"template"\"text"
-val th = x\"template"\"html"
 
 def getTemplateMap(xmlseq: NodeSeq) = {
     val attrList = xmlseq map (x => Map((x\"@k").text -> (x\"@v").text))
     attrList reduceLeft ((x:Map[String,String],y:Map[String,String]) => x++y)
 }
 
-def getItemsMap(xmlseq: NodeSeq) = xmlseq map getTemplateMap(_)
+def getItemsMap(items: NodeSeq) = items.toList map (item => getTemplateMap(item\"map") )
 
-def getTemplateBlocks(blocks: NodeSeq) = {
-    blocks foreach (block => {
-        val blockname = block\"@name"
-        val items = getItemsMap(block\"item")
+def getTemplateBlocks(blocks: NodeSeq) = blocks.toList map (block => (block\"@name", getItemsMap(block\"item"))) 
 
-    val attrList = xmlseq map (x => Map((x\"@k").text -> (x\"@v").text))
-    attrList reduceLeft ((x:Map[String,String],y:Map[String,String]) => x++y)
-}
+val tt = (x\"template"\"text").text
+val th = (x\"template"\"html").text
 
-
-val tupleD = x\"iterations"\"unit" map (unit => (
+val tupleD = (x\"iterations"\"unit" map (unit => (
  (unit\"@email").text,
  getTemplateMap(unit\"map"),
  getTemplateBlocks(unit\"block")
+)) ).toList
 
- val templateMap = unit\"map" reduceLeft ( m => println((m\"@k").text + "=" + (m\"@v").text) )
- unit\"block" foreach (b => println((b\"@name").text) )                  
-}) 
-
-
+println(tt, th, tupleD)
 
 case class RiddanceData (
     val templateText: String, 
